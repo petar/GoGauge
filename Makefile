4567a17a9453a9320a68dac5dc091fb0621680aa
Petar Maymounkov <petarm@gmail.com>
2011-09-21 18:36:46 -0400

    ok

--- Makefile
+++ Makefile
@@ -23,7 +23,7 @@ DIRS=\
 	pprof\
 	unclosed\
 	context\
-	tag\
+	dyna\
 
 TEST=\
 	$(filter-out $(NOTEST),$(DIRS))
---
+++ dyna/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2011 Petar Maymounkov. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+	dyna provides a Dynamic Annotations facility for Go programs,
+	which is currently being used to implement selective logging.
+*/
+package dyna
